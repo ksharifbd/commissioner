7f07d1768f0c00de5c948d9597cffa13cbc2f693
ksharifbd <ksharifbd@gmail.com>
2020-04-14 19:03:51 +0600

    enables transaction grouping by other types

--- src/input-processors/group-transactions-by-week-ids.js
+++ src/input-processors/group-transactions-by-week-ids.js
@@ -1,10 +1,10 @@
-function groupTransactionByWeekids(data) {
+function groupTransactionByWeekids(data, type = 'cash_out_natural') {
   return data.reduce((acc, curr) => {
     if (!Object.prototype.hasOwnProperty.call(acc, curr.week_id)) {
       acc[curr.week_id] = [];
     }
 
-    if (curr.transaction_id.includes('cash_out_natural')) {
+    if (curr.transaction_id.includes(type)) {
       acc[curr.week_id].push({
         transaction_id: curr.transaction_id,
         amount: curr.operation.amount,
