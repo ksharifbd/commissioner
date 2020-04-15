96776295cdf4e85fb7dce2e25e121abdba16db0d
ksharifbd <ksharifbd@gmail.com>
2020-04-15 12:23:30 +0600

    enriches test cases

--- src/commission-calculators/__tests__/attach-commission-to-groups.spec.js
+++ src/commission-calculators/__tests__/attach-commission-to-groups.spec.js
@@ -7,8 +7,39 @@ const {
 
 describe('Attach Commission To Groups', () => {
   const commissionedGroups = attachCommissionToGroups(configs.cashOutNatural, input);
+  const weeksFromGroupedTransaction = Object.keys(input);
+  const weeksFromCommissionedGroups = Object.keys(commissionedGroups);
+  const properties = ['weeklyGross', 'commissionableAmount', 'commission', 'applyNextCommissionOn'];
+  const sampleGroupedTransactions = input[weeksFromGroupedTransaction[3]];
+  const sampleCommissionedTransactions = commissionedGroups[weeksFromCommissionedGroups[2]];
 
   it('should properly attach commissions to groups', () => {
     expect(commissionedGroups).toEqual(output);
+  });
+
+  it('should not contain commissions for weeks which have no transactions', () => {
+    expect(weeksFromCommissionedGroups).not.toContain(weeksFromGroupedTransaction[0]);
+  });
+
+  properties.forEach(property => {
+    it(`should contain ${property} in the transactions for a given week`, () => {
+      expect(sampleCommissionedTransactions[0]).toHaveProperty(property);
+    });
+  });
+
+  it(`should not apply weekly waived amount ${configs.cashOutNatural.week_limit.amount} for the transaction if it does not exceed the limit`, () => {
+    expect(sampleCommissionedTransactions[0].commissionableAmount).toBe(0);
+  });
+
+  it(`should apply weekly waived amount ${configs.cashOutNatural.week_limit.amount} if the transaction amount is equal or greater than that`, () => {
+    expect(sampleCommissionedTransactions[3].commissionableAmount).toBe(
+      sampleCommissionedTransactions[3].weeklyGross - configs.cashOutNatural.week_limit.amount
+    );
+  });
+
+  it(`should not apply weekly waived amount ${configs.cashOutNatural.week_limit.amount} for the transaction if it is already applied in a given week`, () => {
+    expect(sampleCommissionedTransactions[4].commissionableAmount).toBe(
+      sampleGroupedTransactions[4].amount
+    );
   });
 });
--- src/input-processors/__tests__/group-transactions-by-week-ids.spec.js
+++ src/input-processors/__tests__/group-transactions-by-week-ids.spec.js
@@ -4,9 +4,27 @@ const {input, groupedTransactionsByWeek: output} = require('../../mock-data');
 
 describe('Group Transactions By Week Ids', () => {
   const markedTransactionsByIds = markTransactionByIds(input);
-  const groupedTransactionsByWeekIds = groupTransactionsByWeekIds(markedTransactionsByIds);
+  const groupedTransactionsByWeekIds = groupTransactionsByWeekIds(
+    markedTransactionsByIds,
+    'cash_out_natural'
+  );
+  const weeks = Object.keys(groupedTransactionsByWeekIds);
 
   it('should properly group transactions by week ids', () => {
     expect(groupedTransactionsByWeekIds).toEqual(output);
+  });
+
+  it('should have the week_id as key', () => {
+    expect(weeks).toContain(markedTransactionsByIds[0].week_id);
+  });
+
+  it('should contain an empty array for the week which have no transaction of the given operation type', () => {
+    expect(groupedTransactionsByWeekIds[weeks[0]].length).toBeFalsy();
+    expect(groupedTransactionsByWeekIds[weeks[0]].length).toBe(0);
+  });
+
+  it('should contain transactions in the array for the week which have transactions of the given operation type', () => {
+    expect(groupedTransactionsByWeekIds[weeks[1]].length).toBeTruthy();
+    expect(groupedTransactionsByWeekIds[weeks[1]].length).toBe(2);
   });
 });
